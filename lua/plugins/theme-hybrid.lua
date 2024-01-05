3bf78186b523a20bb294d39d3394509b995edbd8
opospisil <ondrej.pospisil06@gmail.com>
2024-01-05 15:10:07 +0100

    working scala workspace

---
+++ lua/plugins/theme-hybrid.lua
@@ -0,0 +1,6 @@
+return {
+	"w0ng/vim-hybrid",
+	name = "hybrid",
+	lazy = false,
+	priority = 999,
+}
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -4,7 +4,6 @@ return {
 	lazy = false,
 	priority = 999,
 	config = function()
-		vim.cmd("colorscheme catppuccin")
 		require("catppuccin").setup({})
 	end,
 }
